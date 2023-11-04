0079a4b2459256c1967ff0e4a7dc9ad2141b2fb5
Sequoyah <quoyahhs@hotmail.com>
2023-11-04 02:52:25 -0400

    Edited README.md

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-/* Add your JavaScript to this file */
+const form = document.querySelector('form');
+const messageDiv = document.querySelector('.message');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault(); 
+
+    const emailInput = document.querySelector('#email');
+    const emailAddress = emailInput.value;
+
+    if (!emailAddress || !isValidEmail(emailAddress)) {
+        messageDiv.textContent = 'Please enter a valid email address.';
+    } else {
+        messageDiv.textContent = `Thank you! Your email address ${emailAddress} has been added to our mailing list!`;
+    }
+
+    emailInput.value = '';
+});
+
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
